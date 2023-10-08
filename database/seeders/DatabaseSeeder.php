c1108916f6363385f7a4dda92b70546f68ba8be6
uvuv <uvuv.space@gmail.com>
2023-10-08 16:39:10 +0300

    Fixed seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,11 +3,13 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Database\Seeders\CustomSeeders\AdminSeeder;
 use Database\Seeders\CustomSeeders\ColorSeeder;
 use Database\Seeders\CustomSeeders\MaterialSeeder;
 use Database\Seeders\CustomSeeders\PricePercentSeeder;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,6 +19,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::query()->create(
+            [
+                'name' => 'dev',
+                'email' => 'dev@gmail.com',
+                'password' => Hash::make('5NI15o32fJ5g')
+            ]
+        );
         $this->call([
             MaterialSeeder::class,
             ColorSeeder::class,
